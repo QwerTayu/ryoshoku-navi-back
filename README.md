3f3d1f940c77fee6e3bca620df20fbc2cec4128d
QwerTayu <tayu.sa99hire.make@gmail.com>
2024-10-14 11:34:13 +0900

    ✨ リッチメニューを作成

--- README.md
+++ README.md
@@ -32,3 +32,96 @@ https://helloworld-2ufsha4fmq-uc.a.run.app/helloworld
 
 
 https://firebase.google.com/docs/functions/get-started?hl=ja&authuser=2&_gl=1*1qsg2ww*_up*MQ..*_ga*OTEzMjA3NzkyLjE3MjcwOTc5ODg.*_ga_CW55HF8NVT*MTcyODI1NDA3OS43LjEuMTcyODI1NDI4Mi41Mi4wLjA.&gen=2nd
+
+
+下記コマンドで環境変数を設定
+```
+$ firebase functions:config:set line.channel_secret="channelSecret"
+
+$ firebase functions:config:set line.channel_access_token="channelAccessToken"
+```
+
+
+
+リッチメニュー
+```
+curl -v -X POST https://api.line.me/v2/bot/richmenu -H 'Authorization: Bearer {チャンネルアクセストークン}' -H 'Content-Type: application/json' -d '{
+    "size": {
+      "width": 2500,
+      "height": 1686
+    },
+    "selected": false,
+    "name": "MENU",
+    "chatBarText": "Tap here",
+    "areas": [
+      {
+        "bounds": {
+          "x": 0,
+          "y": 0,
+          "width": 833,
+          "height": 843
+        },
+        "action": {
+          "type": "postback",
+          "data": "pien"
+        }
+      },
+      {
+        "bounds": {
+          "x": 834,
+          "y": 0,
+          "width": 1666,
+          "height": 843
+        },
+        "action": {
+          "type": "postback",
+          "data": "RECORD"
+        }
+      },
+      {
+        "bounds": {
+          "x": 0,
+          "y": 844,
+          "width": 833,
+          "height": 843
+        },
+        "action": {
+          "type": "postback",
+          "data": "MENU"
+        }
+      },
+      {
+        "bounds": {
+          "x": 834,
+          "y": 844,
+          "width": 833,
+          "height": 843
+        },
+        "action": {
+          "type": "postback",
+          "data": "CHECK"
+        }
+      },
+      {
+        "bounds": {
+          "x": 1668,
+          "y": 844,
+          "width": 833,
+          "height": 843
+        },
+        "action": {
+          "type": "postback",
+          "data": "SETTING"
+        }
+      }
+    ]
+}'
+```
+
+```
+curl --http1.1 -v -X POST https://api-data.line.me/v2/bot/richmenu/richmenu-リッチメニューアイディー/content -H 'Authorization: Bearer {チャンネルアクセストークン}' -H 'Content-Type: image/jpeg' -T richmenu.png
+```
+
+```
+curl -v -X POST https://api.line.me/v2/bot/user/all/richmenu/richmenu-リッチメニューアイディー -H 'Authorization: Bearer {チャンネルアクセストークン}'
+```
--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,84 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions/v1');
+const line = require('@line/bot-sdk');
+// import * as admin from 'firebase-admin';
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// admin.initializeApp();
+// const db = admin.firestore();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const config = {
+  channelAccessToken: functions.config().line.channel_access_token,
+  channelSecret: functions.config().line.channel_secret
+};
 
-exports.helloWorld = onRequest({
-    maxInstances: 1,         // 最大同時実行数
-    timeoutSeconds: 30,      // タイムアウト秒数
-    memory: "128MiB"         // メモリサイズ
-  }, (request, response) => {
-    logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase with v2 settings!");
-  });
+const client = new line.Client(config);
+
+exports.lineWebhook = functions
+    .region("asia-northeast1")
+    .https.onRequest(async (request, response) => {
+      const signature = request.get("x-line-signature");
+
+      if (!signature || !line.validateSignature(
+          request.rawBody, config.channelSecret, signature
+      )) {
+        throw new
+        line.SignatureValidationFailed(
+            "signature validation failedで失敗ですわ！",
+            signature
+        );
+      }
+      const res = request.body.events[0];
+      
+      try {
+        await actionPien(res);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+const actionPien = async (event) => {
+  const userId = event.source.userId;
+  if (event.type !== 'postback') {
+    return;
+  }
+    try {
+      const message = [
+        {
+          type: "text",
+          text: event.postback.data
+        },
+        {
+          type: "text",
+          text: "ぴえん"
+        }
+      ];
+      client.replyMessage(event.replyToken, message);
+      // return addPien(userId, event.timestamp);
+    } catch (error) {
+      console.error(JSON.stringify(error));
+      return Promise.resolve(null);
+    }
+};
+
+// const addPien = async (userId, timestamp) => {
+//   const pienDate = timestampToTime(timestamp);
+//   const pienEvent = {
+//     pienTime: pienDate,
+//   };
+  
+//   return db.collection(`users/${userId}/piens`).add(pienEvent);
+// }
+
+// 引数 timestamp の単位はミリ秒
+const timestampToTime = (timestamp) => {
+  const date = new Date(timestamp);
+  const yyyy = `${date.getFullYear()}`;
+  // .slice(-2)で文字列中の末尾の2文字を取得する
+  // `0${date.getHoge()}`.slice(-2) と書くことで０埋めをする
+  const MM = `0${date.getMonth() + 1}`.slice(-2); // getMonth()の返り値は0が基点
+  const dd = `0${date.getDate()}`.slice(-2);
+  const HH = `0${date.getHours() + 9}`.slice(-2); // 日本時間になるよう+9h
+  const mm = `0${date.getMinutes()}`.slice(-2);
+  const ss = `0${date.getSeconds()}`.slice(-2);
+
+  return `${yyyy}/${MM}/${dd} ${HH}:${mm}:${ss}`;
+}
